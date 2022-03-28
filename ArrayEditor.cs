6391b2e0b9c8c19e7d6f3f431b26eff5e312690e
ELS122 <54604728+ELS122@users.noreply.github.com>
2022-03-28 20:49:48 +0300

    Add files via upload

---
+++ ArrayEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+public static class Extensions
+{
+	/// <summary>
+	/// Inserts thing into array, increasing the size of array by 1
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
+	/// <param name="item"></param>
+	/// <param name="placement">if not set, will add onto the end of array</param>
+	/// <returns>Returns edited array</returns>
+	public static T[] Insert<T>(this T[] array, T item, int placement = -1)
+	{
+		if (array == null)
+			return new T[] { item };
+
+		if (placement < 0)
+			placement = array.Length;
+
+		placement = Mathf.Clamp(placement, 0, array.Length);
+		Array.Resize(ref array, array.Length + 1);
+		for (int i = array.Length - 1; i > placement; i--)
+			array[i] = array[i - 1];
+
+		array[placement] = item;
+
+		return array;
+	}
+}
