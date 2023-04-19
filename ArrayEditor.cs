b2f19d4659b23a1df90407e97fceee2e98bd3583
ELS122 <54604728+ELS122@users.noreply.github.com>
2023-04-19 12:28:36 +0300

    Update ArrayEditor.cs
    Improved code, less resource intensive. seamless update, code using the previous version will work fine with this.

--- ArrayEditor.cs
+++ ArrayEditor.cs
@@ -4,28 +4,35 @@ using System.Linq;
 public static class Extensions
 {
 	/// <summary>
-	/// Inserts thing into array, increasing the size of array by 1
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="array"></param>
-	/// <param name="item"></param>
-	/// <param name="placement">if not set, will add onto the end of array</param>
-	/// <returns>Returns edited array</returns>
-	public static T[] Insert<T>(this T[] array, T item, int placement = -1)
-	{
-		if (array == null)
-			return new T[] { item };
+        /// Inserts thing into array, increasing the size of array by 1
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="item"></param>
+        /// <param name="placement">if not set, will add onto the end of array</param>
+        /// <returns>Returns edited array</returns>
+        public static T[] Insert<T>(this T[] array, T item, int placement = -1)
+        {
+            if (array == null)
+                return new T[] { item };
 
-		if (placement < 0)
-			placement = array.Length;
+            int length = array.Length;
+            int newLength = length + 1;
 
-		placement = Mathf.Clamp(placement, 0, array.Length);
-		Array.Resize(ref array, array.Length + 1);
-		for (int i = array.Length - 1; i > placement; i--)
-			array[i] = array[i - 1];
+            if (placement < 0 || placement >= length)
+                placement = length;
 
-		array[placement] = item;
+            T[] newArray = new T[newLength];
+            int i = 0;
 
-		return array;
-	}
+            for (; i < placement; i++)
+                newArray[i] = array[i];
+
+            newArray[i] = item;
+
+            for (; i < length; i++)
+                newArray[i + 1] = array[i];
+
+            return newArray;
+        }
 }
